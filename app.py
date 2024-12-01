74fc7d6babc789f4b4e2145ba909afeab7f108ea
farhan <farhankazai1@gmail.com>
2024-12-01 23:44:31 +0700

    .

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import redis
 from datetime import datetime
 import pytz
 import os
+from io import BytesIO
+import qrcode
+import fitz
+import threading
 
 
 app = Flask(__name__)
@@ -23,6 +27,7 @@ pdf_stemped = './pdf_stemped/'
 created_at = str(datetime.now(pytz.timezone(
     'Asia/Jakarta')).strftime('%Y%m%d%H%M%S'))
 qr_id = str(uuid.uuid4())
+DEFAULT_QR_SIZE = 100
 
 
 def download_file(id, pdf_url):
@@ -37,6 +42,50 @@ def download_file(id, pdf_url):
                 f"Failed to download file. Status code: {response.status_code}")
     except Exception as e:
         raise RuntimeError(f"Failed to download PDF. Error: {e}")
+
+
+def generate_qr(qr_url: str) -> BytesIO:
+    qr_code = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr_code.add_data(qr_url)
+    qr_code.make(fit=True)
+    qr_image = qr_code.make_image()
+
+    img_byte_array = BytesIO()
+    qr_image.save(img_byte_array)
+    img_byte_array.seek(0)
+    return img_byte_array
+
+
+def stemp_qr(pdf_path: str, qr_path: BytesIO, qr_position_x: int, qr_position_y: int, output_pdf_stemped: str):
+    try:
+        pdf_document = fitz.open(pdf_path)
+    except Exception as e:
+        raise FileNotFoundError(
+            f"Failed to open PDF file: {pdf_path}. Error: {e}")
+
+    try:
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document[page_num]
+            page.insert_image(
+                fitz.Rect(
+                    qr_position_x,
+                    qr_position_y,
+                    qr_position_x + DEFAULT_QR_SIZE,
+                    qr_position_y + DEFAULT_QR_SIZE,
+                ),
+                stream=qr_path,
+            )
+
+        pdf_document.save(output_pdf_stemped)
+    except Exception as e:
+        raise RuntimeError(f"Failed to modify the PDF. Error: {e}")
+    finally:
+        pdf_document.close()
 
 
 def insert_to_redis(id, qr_url, pdf_url, qr_position_x, qr_position_y, api_callback):
@@ -84,6 +133,92 @@ def collect_data():
                 "QR_ID": qr_id
             }
         }), 200
+    except Exception as e:
+        return jsonify({"OUT_STAT": "ERROR", "MESSAGE": str(e)}), 500
+
+
+@app.route('/stemp_pdf', methods=['POST'])
+def stemp():
+    try:
+        updated_at = str(datetime.now(pytz.timezone(
+            'Asia/Jakarta')).strftime('%Y%m%d%H%M%S'))
+        data = request.json
+        data_key = data.get("DATA_KEY")
+
+        if not data_key:
+            return jsonify({"OUT_STAT": "ERROR", "MESSAGE": "DATA_KEY is required"}), 400
+
+        redis_data = REDIS_DB.hgetall(data_key)
+        if not redis_data:
+            return jsonify({"OUT_STAT": "ERROR", "MESSAGE": "Data not found in Redis"}), 404
+
+        id = redis_data.get("ID")
+        qr_url = redis_data.get("QR_URL")
+        path_source = redis_data.get("PATH_SOURCE")
+        qr_position_x = int(redis_data.get("QR_POSITION_X"))
+        qr_position_y = int(redis_data.get("QR_POSITION_Y"))
+        api_callback = redis_data.get("API_CALLBACK")
+
+        qr_path = generate_qr(qr_url)
+        output_pdf_stemped = f"{pdf_stemped}{updated_at}_{id}_qr_stemped.pdf"
+        stemp_qr(path_source, qr_path, qr_position_x,
+                 qr_position_y, output_pdf_stemped)
+
+        REDIS_DB.hset(data_key, "UPDATED_AT", updated_at)
+        REDIS_DB.hset(data_key, "PATH_STEMPED", output_pdf_stemped)
+        REDIS_DB.hset(data_key, "STATUS_STEMPED", "SUCCESS")
+        REDIS_DB.hset(data_key, "FLAG", "Y")
+
+        if api_callback and api_callback.strip():
+            callback_url = f"{api_callback}?id={id}"
+            callback_response = requests.get(callback_url)
+
+        return jsonify({
+            "OUT_STAT": "SUCCESS",
+            "OUT_DATA": {
+                "PATH_STEMPED": output_pdf_stemped
+            }
+        }), 200
+
+    except Exception as e:
+        return jsonify({"OUT_STAT": "ERROR", "MESSAGE": str(e)}), 500
+
+
+@app.route('/get_pdf', methods=['POST'])
+def get_pdf():
+    try:
+        data = request.json
+        data_key = data.get("QR_ID")
+
+        if not data_key:
+            return jsonify({"OUT_STAT": "ERROR", "MESSAGE": "DATA_KEY is required"}), 400
+
+        redis_data = REDIS_DB.hgetall(data_key)
+        if not redis_data:
+            return jsonify({"OUT_STAT": "ERROR", "MESSAGE": "Data not found in Redis"}), 404
+
+        path_stemped = redis_data.get("PATH_STEMPED")
+        if not path_stemped:
+            return jsonify({"OUT_STAT": "ERROR", "MESSAGE": "Path stemped not found in Redis"}), 404
+
+        download_url = f"http://{request.host}/download/{os.path.basename(path_stemped)}"
+
+        return jsonify({
+            "OUT_STAT": "SUCCESS",
+            "OUT_DATA": {
+                "PATH_STEMPED_URL": download_url
+            }
+        }), 200
+
+    except Exception as e:
+        return jsonify({"OUT_STAT": "ERROR", "MESSAGE": str(e)}), 500
+
+
+@app.route('/download/<filename>', methods=['GET'])
+def download_pdf(filename):
+    try:
+        return send_from_directory(pdf_stemped, filename, as_attachment=True)
+
     except Exception as e:
         return jsonify({"OUT_STAT": "ERROR", "MESSAGE": str(e)}), 500
 
